45a5ce1bff4ee85933e772128e3ef9a9ecf0fdf0
tabeGH <tarek.bermalm@knightec.se>
2024-04-25 14:13:47 +0200

    desc

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,13 @@
 import { loadFlash } from 'sveltekit-flash-message/server';
 import { savedTable, type Saved } from '../lib/server/database/drizzle-schemas';
 import db from '$lib/server/database/drizzle';
-import { asc } from 'drizzle-orm';
+import { desc } from 'drizzle-orm';
 
 export const load = loadFlash(async (event) => {
 	const savedFilms: Saved[] = await db
 		.selectDistinct()
 		.from(savedTable)
-		.orderBy(asc(savedTable.createdAt));
+		.orderBy(desc(savedTable.createdAt));
 
 	return { user: event.locals.user, savedFilms };
 });
